36e9fbe4a1223ac7bad2f864af934fa019663750
Ruslan Hrynov <androstev49@gmail.com>
2022-09-16 14:22:53 +0200

    Add "WebAplication1"

---
+++ WebApplication1/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace GitHubOAuth2Grenka.Controllers
+{
+    [Route("[controller]/[action]")]
+
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
