8467258e5893538cc3b0c785e0956c0cd61b9cf3
Ruslan Hrynov <androstev49@gmail.com>
2022-09-19 14:24:18 +0200

    Add "testApi"

---
+++ testApiRequest/Program.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace testApiRequest
+{      
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+
+         var repositories =  await RunAsync();
+
+            Console.WriteLine("Twoje repozytorium: ");
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine("Obserwatorzy " + repo.Watchers);
+                Console.WriteLine();
+            }
+
+            
+        }
+
+       
+        static async Task<List<Repository>> RunAsync()
+        {
+
+            string userName;
+            Console.WriteLine("Wpisz username konta github");
+            userName = Console.ReadLine();
+            
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/users/"+userName+"/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+
+            return repositories;
+
+          
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
---
+++ testApiRequest/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace testApiRequest
+{
+   public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+      
+
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+      
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+    }
+}
