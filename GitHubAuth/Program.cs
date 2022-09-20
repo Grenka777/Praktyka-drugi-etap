b21064a137a2df762b9d576d6d04c9cfc4b54a51
Ruslan Hrynov <androstev49@gmail.com>
2022-09-20 10:58:56 +0200

    Add projekt "Githubauth"

---
+++ GitHubAuth/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GitHubAuth
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+       static Root root;
+        static async Task Main(string[] args)
+        {
+            Task.WaitAll(RunAsync());
+            
+
+            var repositories = await RunGitRepo();
+            Console.WriteLine("Twoje repozytorium:\t");
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine("Nazwa: "+repo.Name);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine("Obserwatorzy " + repo.Watchers);
+                Console.WriteLine("\t");
+            }
+
+        }
+
+
+            static async Task RunAsync()
+            { 
+            var token = string.Empty;
+            Console.WriteLine("Wpisz personal access token GitHub ");
+            token = Console.ReadLine();
+
+
+
+            client.BaseAddress = new Uri("https://api.github.com");
+            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("AppName", "1.0"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+
+            var response = await client.GetAsync("/user");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+             root = JsonSerializer.Deserialize<Root>(content);
+
+            Console.WriteLine("User: " + root.Login);
+
+            }
+
+
+           static async Task<List<Repository>> RunGitRepo() 
+        {
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            
+            var streamTask = client.GetStreamAsync(root.ReposUrl);
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+
+            return repositories;
+
+
+        }
+
+
+
+
+
+
+
+        
+    }
+}
---
+++ GitHubAuth/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace GitHubAuth
+{
+    class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+
+
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+    }
+}
